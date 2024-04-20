2f7f6e3459e8aa0cfe4ad8466e230b114878d189
AnaghaRw <anagha.r.warrier@gmail.com>
2024-04-20 16:49:15 +0530

    sidebar

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Sidebar.css'
+import {assets} from '../../assets/assets'
+
+const Sidebar = () => {
+
+    const [extended, setExtended] = useState(false) //to give functionality where menu opens sidebar
+
+  return (
+    <div className='sidebar'>
+        <div className="top">
+            <img className='menu' src={assets.menu_icon} />
+            <div className="new-chat">
+                <img src={assets.plus_icon} />
+                <p>New Chat</p>
+            </div>
+            <div className="recent">
+                <p className="recent-title">Recent</p>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} />
+                    <p>What is react ... </p>
+                </div>
+            </div>
+        </div>
+        <div className="bottom">
+            <div className="bottom-item recent-entry">
+                <img src={assets.question_icon} />
+                <p>Help</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.history_icon} />
+                <p>Activity</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.settings_icon} />
+                <p>Settings</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+import menu_icon from './menu.png'
+import plus_icon from './plus.png'
+import history_icon from './history.png'
+import message_icon from './message.png'
+import settings_icon from './settings.png'
+import question_icon from './help.png'
+
+
+export const assets = {
+    menu_icon,
+    plus_icon,
+    history_icon,
+    message_icon,
+    settings_icon,
+    question_icon
+}
