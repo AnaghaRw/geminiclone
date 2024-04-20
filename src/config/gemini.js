d7b65b80318d390ff9cf5c8b3fa8390ad0028030
AnaghaRw <anagha.r.warrier@gmail.com>
2024-04-20 19:55:48 +0530

    api

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -8,7 +8,7 @@ import {
   } from  "@google/generative-ai" ;
   
   const MODEL_NAME = "gemini-1.0-pro";
-  const API_KEY = "AIzaSyCQBjhJCWimrCdq_hKbFukDf9zs7ZrYs5g";
+  const API_KEY = "AIzaSyBeItx64OD101lKA6oOvfzhSRiCChpqZIw";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
